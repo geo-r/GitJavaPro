1c70e3d23c8341f6b990244e1e303eabfbe5e13f
geo-r <ge0maret@gmail.com>
2024-05-21 19:44:03 +0300

    all files 21/5

--- src/gr/aueb/cf/Hello.java
+++ src/gr/aueb/cf/Hello.java
@@ -3,5 +3,6 @@ package gr.aueb.cf;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello!");
+        System.out.println("hello coding factory");
     }
 }
